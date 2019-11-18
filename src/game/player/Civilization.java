f61185ce96fc4e0b380340cc4cfa3de7fe3be321
Yusuf Avci <yusufamca90@gmail.com>
2019-11-18 20:19:06 +0300

    Added new civilization

--- src/game/player/Civilization.java
+++ src/game/player/Civilization.java
@@ -20,8 +20,20 @@ public class Civilization {
             cityCost = new Resource(2, 0, 0, 0, 3);
             devCardCost = new Resource(0, 0, 1, 0, 2);
         }
+        else if(civilization.equals(CivilizationEnum.GB)) {
+            roadCost = new Resource(0, 2, 0, 0, 0);
+            villageCost = new Resource(0, 1, 1, 1, 1);
+            cityCost = new Resource(2, 0, 0, 0, 2);
+            devCardCost = new Resource(1, 0, 1, 1, 0);
+        }
+        else if(civilization.equals(CivilizationEnum.MAYA)) {
+            roadCost = new Resource(1, 1, 0, 1, 1);
+            villageCost = new Resource(0, 1, 1, 1, 0);
+            cityCost = new Resource(0, 2, 0, 1, 3);
+            devCardCost = new Resource(0, 2, 1, 0, 0);
+        }
     }
 
-    public enum CivilizationEnum {SPAIN, OTTOMANS};
+    public enum CivilizationEnum {SPAIN, OTTOMANS, GB, MAYA};
 
 }
