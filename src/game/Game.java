6f7ac384f091aff977a3d04e7855e71ee6bc2feb
Elif Demir <elifdemir@protonmail.com>
2019-12-21 18:31:24 +0300

    The price for activating the pirate is added.

--- src/game/Game.java
+++ src/game/Game.java
@@ -215,6 +215,7 @@ public class Game implements Serializable {
                     break;
                 case ROAD_BUILDING:
                     roadsBuilt = Math.min(roadsBuilt-2,-2);
+                    break;
             }
             return true;
         }
